43b974805fde2a458551d20e96fe655066c29c46
Ramtin <104753827+Mr-Ramtin@users.noreply.github.com>
2022-07-29 01:40:14 +0430

    Add files via upload

---
+++ Filtering.py
@@ -0,0 +1,358 @@
+# In The Name Of Allah 
+# Coded By ==> Ramtin Abdali
+
+#import librari's'
+
+import socket
+server = socket.gethostname()
+ip=socket.gethostbyname(server)
+print ("server: {0}".format(server))
+print ("IP: {0}".format(ip))
+
+import os 
+
+#auro install librari's
+
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+os.system("pip install BeautifulTable")
+os.system("pkg install figlet")
+
+#importing libs
+
+from beautifultable import BeautifulTable
+import time                                          
+import sys                                                      
+import colored                                                  
+import webbrowser
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+import random
+from colorama import *       
+              
+# code colors
+
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+
+print(f"{rang} ")
+
+print (Fore.WHITE + "")
+
+
+print (Fore.YELLOW + "")
+K = """ [*] coded by ramtin abdali,...
+
+                                                       
+
+"""
+for c in K:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+
+print(f"{blue} ")
+d = """'
+
+
+
+░█████╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗██████╗░███████╗░██████╗░░█████╗░██╗██╗░░░░░
+██╔══██╗██╔══██╗████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝██╔════╝░██╔══██╗██║██║░░░░░
+███████║██████╔╝██╔████╔██║███████║██╔██╗██║██████╦╝█████╗░░██║░░██╗░███████║██║██║░░░░░
+██╔══██║██╔══██╗██║╚██╔╝██║██╔══██║██║╚████║██╔══██╗██╔══╝░░██║░░╚██╗██╔══██║██║██║░░░░░
+██║░░██║██║░░██║██║░╚═╝░██║██║░░██║██║░╚███║██████╦╝███████╗╚██████╔╝██║░░██║██║███████╗
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝░╚═════╝░╚═╝░░╚═╝╚═╝╚══════╝
+
+
+
+
+
+███████████████████████████████████████████████████████████████████████████████████████▀█
+█▄─▄███▄─▄▄─██▀▄─██▄─▄▄▀█▄─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▄█▄─▄███─▄─▄─█▄─▄▄─█▄─▄▄▀█▄─▄█▄─▀█▄─▄█─▄▄▄▄█
+██─██▀██─▄█▀██─▀─███─██─██─▄█▀██─▄─▄██─▄████─███─██▀███─████─▄█▀██─▄─▄██─███─█▄▀─██─██▄─█
+▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▀▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀
+
+
+
+░░░░░░░░░░░░░░░░▓██████▓▓▓░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░█████▓▓█████████▓░░░░░░░░░░░
+░░░░░░░░░░█████▓░░▓█████████████░░░░░░░░░
+░░░░░░░░▓███▓░░░▓█████████████████░░░░░░░
+░░░░░░░███▓░░░░░███████████████████▓░░░░░
+░░░░░░███░░░░░░██████████████████████░░░░
+░░░░░███░░░░░░░███████████████████████░░░
+░░░░███░░░░░░░░███████░░░░██████████▓█▓░░
+░░░███▓░░░░░░░░███████░░░░▓██████████▓█░░
+░░▓███░░░░░░░░░░██████▓░░▓███████████▓██░
+░░████░░░░░░░░░░▓████████████████████▓▓█░
+░▓█░█▓░░░░░░░░░░░░████████████████████░██
+░██░█░░░░░░░░░░░░░░▓██████████████████░██
+░█▓░█░░░░░░░░░░░░░░░░░▓███████████████░▓█
+▓█▓░█▓░░░░░░░░░░░░░░░░░░██████████████░░█
+██░░██░░░░░░░░░░░▓▓░░░░░░▓████████████░░█
+██░▓░█░░░░░░░░░░████▓░░░░░███████████▓░░█
+██░█░██░░░░░░░░▓█████░░░░░░██████████░░▓█
+██░▓█░██░░░░░░░░████▓░░░░░░█████████░░▓▓█
+▓█░░█░░█▓░░░░░░░░▓▓░░░░░░░░████████▓░░▓██
+░█░░█▓░▓██░░░░░░░░░░░░░░░░░███████▓░░█▓█▓
+░██░███████▓░░░░░░░░░░░░░░██████████▓█▓█░
+░▓█░██▓░░░▓███░░░░░░░░░░▓██████▓░░░▓██▓█░
+░░███▓░░░░░░░███████████████▓░░░░░░░░██▓░
+░░░██░░▓▓█▓▓▓░░░▓████████▓░░░░▓▓█▓▓░░██░░
+░░░▓█░████████▓░░░░░░░░░░░░▓████████░▓█░░
+░░░█▓▓███████████░░░░░░░░████████████░█▓░
+░░░█░█████████████░░░░░░█████████████░██░
+░░▓█░▓████████████░░░░░░█████████████░░█░
+░░▓█░▓▓███████████░░░░░░███████████▓▓░░█░
+░░▓█░░░▓█████████░░░░░░░░█████████▓░░░░█░
+░░░█▓░░░████████░░░░░░░░░░████████░░░░██░
+░░░██░░░░░█████░░░░████░░░░█████▓░░░░░█▓░
+░░░░██░░░░░░░░░░░░██████░░░░░░░░░░▓░░██░░
+░░░░▓████▓░░░░░░░░███▓██▓░░░░░░░░█████░░░
+░░░░░▓█▓████▓░░░░░██░▓▓██░░░░▓████░██░░░░
+░░░░░░░░▓█▓██░▓░░░██▓▓▓██░░█▓█████░░░░░░░
+░░░░░░░░▓█░███▓░░░▓▓▓░▓░░░░░█▓██▓█░░░░░░░
+░░░░░░░░▓█░██▓░░▓░░░░░░░░░▓░███▓▓█░░░░░░░
+░░░░░░░░▓█░███▓███▓░░░░░▓███▓██░▓█░░░░░░░
+░░░░░░░░██░░██░▓░█████████░▓▓█▓░▓█░░░░░░░
+░░░░░░░░▓█░░▓██▓▓░░▓░█░█░▓░▓██░░░█░░░░░░░
+░░░░░░░░░█▓░░████░▓░░▓░░▓▓███░░░██░░░░░░░
+░░░░░░░░░▓█▓░░████████▓█████░░░██▓░░░░░░░
+░░░░░░░░░░░██░░▓▓▓▓▓▓▓▓▓▓▓█░░░██░░░░░░░░░
+░░░░░░░░░░░░██░░▓█████▓██▓░░▓██░░░░░░░░░░
+░░░░░░░░░░░░░██░░░░░▓▓░░░░░███░░░░░░░░░░░
+░░░░░░░░░░░░░░██░░░░░░░░░░██▓░░░░░░░░░░░░
+░░░░░░░░░░░░░░▓██░░░░░░░░██░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░██████████░░░░░░░░░░░░░░░
+
+
+
+
+"""
+for c in d:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.005)
+print ("""
+
+ARⲘAN`begail{leader~filtering}
+
+""")
+
+webbrowser.open("https://github.com/Mr-Ramtin")
+
+name = input('whats your name ??? enter Your name >>> ')
+os.system("clear")
+print (Fore.GREEN+'['+Fore.RED+'*'+Fore.GREEN+']'+Fore.WHITE+' Hi ðð '+name+' welcome to Rami-reporter >_< ') 
+
+print(f"{red}")
+print("""
+
+　　　　　　　　　　　　　　　　　　　　　　_¶¶
+　　　　　　　　　　　　　　　　　　　　　　　¶¶¶¶
+　　　　　　　　　　　　　　　　　　　　　　¶¶¶¶¶¶¶
+　　　　　　　　　　　　　　　　　　　　　¶¶¶¶¶¶¶¶¶¶¶
+　　　　　　　　　　　　　　　　　　　　_¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　　　　¶¶¶¶¶¶¶¶¶　　　　¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　　_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　_¶¶¶¶¶¶¶_____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　¶¶¶¶¶____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶¶
+　¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　　　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　_¶_　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶　　_¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶¶¶___¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶_¶¶¶¶_¶¶　¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+　¶__¶__¶¶　　_¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶_____¶¶¶　　　　¶¶¶¶¶¶¶
+¶________¶¶¶　　　　¶¶
+¶¶¶¶¶¶¶¶¶¶¶¶¶
+
+
+
+
+
+╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╭╮╭╮
+╭━━┳┳╮┃┃╭━┳━┳━┳━┳┳┳━┫┣╋┫╭━━┳━┳╮
+┃┃┃┃┃┃┃╰┫╋┣╮┃╭┫┻┫╭┫╋┃━┫┣┫┃┃┃┃┃┃
+╰┻┻╋╮┃╰━┻━╯╰━╯╰━┻╯╰━┻┻┻┻┻┻┻┻┻━╯
+╱╱╱╰━╯
+
+""")
+
+print(f"{red}")
+print("""++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++""")
+
+print(f"{blue}")
+up = input("""
+di escript==>[1]
+escript nime feal==>[2]
+escript feal==>[3]
+hit habz==>[4]
+link mokhareb python==>[5]
+link viros==>[6]
+bag==>[7]
+link mokhareb==>[8]
+link sex==>[9]
+server hesas==>[10]
+ping hesas==>[11]
+algoritm hesas==>[12]
+algoritm gadimi==>[13]
+fail server==>[14]
+code random==>[15]
+info==>[16]
+
+
+             ‌            ‌number==>
+""")
+
+
+if up == "1" or up == "di escript":
+	print("€")
+	print(Fore.GREEN+"")
+	print("Inam codet==>")
+	print(Fore.YELLOW+"")
+po=["[#]http://Dxprit.ngopb.hackers65.78000766.infect.in\n[#]http://Dxprit.ngopb.hackers65.7886868766.infect.in\n[#]http://Dxprit.ngopb.hackers65.7876087656.infect.in\n[#]http://Dxprit.ngopb.hackers65.78098755766.infect.in\n[#]http://Dxprit.ngopb.hackers65.70000038766.infect.in\n[#]https://s6.uupload.ir/files/images_yqt3_thumb.jpeg\n"]
+print(random.choice(po))
+	
+if up == "2" or up == "escript nime feal":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.BLUE+"")
+	print("[#]-http://Dxprit.ngopb.hackers65.78766.infect.in")
+	
+if up =="3" or up == "escript feal":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.RED+"")
+	print("[#]-http://Dxprit.nob.hackers65.77678886.infect.in")
+	
+if up == "4" or up == "hit habz":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.WHITE+"")
+	print("[#]http://dl.uploadgram.me/60bdffd3a2be4h.https://cigarettes-chemical-rid-numeric.trycloudflare.com.http://get-500-usd-free-to\n[#]http://Github.com.VuPyrv65.infect.in")
+	
+if up == "5" or up == "link mokhareb python":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.RED+"")
+	print("[#]-https://cigarettes-chemical-rid-numeric.trycloudflare.com\n[#]-https://dl.uploadgram.me/60bdffd3a2be4h\n[#]-http://get-1000-followers-for-instagram@cigarettes-chemical-rid-numeric.trycloudflare.com\n[#]-http://yftt-filter-rubika.hackers923023.dscript.com")
+	
+if up == "6" or up == "link viros":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.YELLOW+"")
+	print("http://ffmodmenu.page.link/Anti-V")
+
+if up == "7" or up == "bag":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.BLUE+"")
+	print("[#]-http://4.2.4.8/f////g.h/4.2.4.6.4.7.8.4.6.5.8.2.3.7.6.0.3.6.2")
+	
+if up == "8" or up == "link mokhareb":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.WHITE+"")
+	print("[#]-https://bit.ly/3ild93L\n[#]-https://bit.ly/3ild93Ljz\n[#]-https://b2n.ir/r58546")
+	
+if up == "9" or up == "link sex":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.YELLOW+"")
+	print("[#]-http://s6.uupload.ir/files/sex.sex.porn.xnxx.ytf=15k_dsdu.jpeg00Filter.rubika.(filter_rubika).https://uupload.ir/view/img-20220315-wa0039_uzz6.jpg")
+	
+if up == "10" or up == "server hesas":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.RED+"")
+	print("[#]-5.1.1.2\n[#]-2.4.0.1\n[#]-4.0.7.5\n[#]-7.8.9.2\n[#]-5.6.8.3\n[#]-3.9.2.3\n[#]-6.0.3.2\n[#]-0.3.4.0\n[#]-7.0.2.5\n[#]-3.8.6.4\n[#]-2.3.0.4")
+	
+if up == "11" or up == "ping hesas":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.BLUE+"")
+	print("[#]-208205\n[#]-187463\n[#]-207497\n[#]-310561\n[#]-157621\n[#]-3749934")
+	
+if up == "12" or up == "algoritm hesas":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.WHITE+"")
+	print("[#]-y//d/f\n[#]-y//d/\n[#]-g/d//\n[#]-d//f/h\n[#]-f//a/y\n[#]-f///g.h\n[#]-/e/f///g.h////\n[#]-/f/g.h////\n[#]-///g///h\n[#]-e////f.g/\n[#]-e////fg/\n[#]-f////gh/\n[#]-f////g.h/")
+	
+if up == "13" or up == "algoritm ghadimi":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.YELLOW+"")
+	print("[#]-(f-h)\n[#]-Filtering_rubika/h")
+	
+		 
+if up == "14" or up == "fail server":
+	print("€")
+	print(Fore.GREEN+"")
+	print("inam codat==>")
+	print(Fore.RED+"")
+	print("[#]-cood filter group 90% apn cood\n[#]-proxy-server\n[#]-cood filter account rubika 87% apn\n[#]-cood filter proxy-port:8000\n[#]-cood filter chaneel rubika 78% apn\n[#]-proxy file port:auth")
+	
+if up == "15" or up == "code random":
+        os.system("figlet MAKHLOT GAR")
+        print (Fore.GREEN +"")
+        os.system("clear") 
+        print("Enter your ip tor : ")
+        code = input()
+        print ("Link site sex : ")
+        sex = input()
+        print ("Link aks sexi : ")
+        aks = input()
+        print ("id target : \n(@Ramtin_Abdali)")
+        targete = input()
+        print("http://1.7.2.5//e/(@supportbot)h//.",sex,"(iran-@support)0.5.4.8.9.sport.-rubika.-spam.(/yftt15k)_1.2.46kpd.9.hfdjsh.98.87.haY7.6.9.8.5.7.6//Fil//com.99999.4.7.1////",targete,".fil.(/yftt15k).sex.porn.huqhttps://s6.",aks,".www.pornhub.com(yftt13k)fil.*rubika.*fil_.rubika.M90.M21.M66.*M9999.(/yftt15k)./@support/f/1.1.1/2.2.2/3.3.",code,"///((/yftt99k)/*fill-*rubika*f*fill-xxx(Gif.sex)(*'*)/rest_122334_filteri.com.hsb_17g.sxs.www.com ")
+
+
+if up == "16" or up == "info":
+        print (Fore.YELLOW+"[info]"+Fore.WHITE+'author > Ramtin - Abdali\n')
+        print (Fore.YELLOW+"[emile]"+Fore.WHITE+'emile > ramtinexploit@gmail.com\n')
+        print (Fore.YELLOW+"[tel.ID]"+Fore.WHITE+'telegram id > @ramtin_abdali\n')
+        print (Fore.YELLOW+"[IG]"+Fore.WHITE+'instagram > @ramtin_abdali\n')
+        print (Fore.YELLOW+'[acaunt.IDs]'+Fore.WHITE+':')
+        print (Fore.YELLOW+'[RB-RUBIKA]' +Fore.RED+':'+ Fore.WHITE+'\n@leader_filteryg\n')
+        print (Fore.YELLOW+"[Number]"+Fore.WHITE+':')
+        print(Fore.YELLOW+'[ARMAN NUMBER]==>'+Fore.WHITE+'\n09932797165\n')
+        
+
+        
+	
+
+	
+	
+	
+	
